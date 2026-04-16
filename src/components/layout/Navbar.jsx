8351771ec426bcf30b3c5dc6995a537ea6e387ae
Utsav Agrawal <utsav111agrawal@gmail.com>
2026-04-16 14:49:13 +0530

    styled navbar using flexbox

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,11 +1,25 @@
-function Navbar(){
-    return (
-        <div>
-            <h2>MyDukaan</h2>
-            <input type="text" placeholder="saaman dhundlo" />
-            <p>Cart (0)</p>
-        </div>
-    );
+function Navbar() {
+  return (
+    <div style={{
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+      padding: "15px 25px",
+      borderBottom: "2px solid #ccc"
+    }}>
+      
+      <h2>MyDukkan</h2>
+
+      <input 
+        type="text" 
+        placeholder="Samaan Dhundlo" 
+        style={{ padding: "10px", width: "200px" }}
+      />
+
+      <p>Cart (0)</p>
+
+    </div>
+  );
 }
 
 export default Navbar;

4ae266be0cc52002d72870751717b0a7cf05609e
Utsav Agrawal <utsav111agrawal@gmail.com>
2026-04-16 14:44:39 +0530

    created basic navbar

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar(){
+    return (
+        <div>
+            <h2>MyDukaan</h2>
+            <input type="text" placeholder="saaman dhundlo" />
+            <p>Cart (0)</p>
+        </div>
+    );
+}
+
+export default Navbar;
